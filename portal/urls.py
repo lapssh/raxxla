1245e89b63bf41a3322d8e06d47106c8f00232a4
Aleksey Lapssh <lapssh@yandex.ru>
2021-03-20 19:53:57 +0300

    добавлены шаблоны: - базовый -футер -хэдер - для модулей

--- portal/urls.py
+++ portal/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from portal.views import index
+from portal.views import index, speech_text_speech, modules
 
 urlpatterns = [
     path('', index, name='index'),
+    path('modules', modules, name='modules'),
+    path('speech_text_speech', speech_text_speech, name='speech_text_speech'),
 ]
